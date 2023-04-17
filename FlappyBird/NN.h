7bb46883c5af3b2d0893fe20aa0ee1375de243d0
Maurice Thompson-Hamilton <mauricemlth@hotmail.com>
2023-04-17 14:39:37 +0100

    added homebrewed matrix class to handle math when applying weights and biases

---
+++ FlappyBird/NN.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef NN_H
+#define NN_H
+
+//Vector variants are avalable
+class NN
+{
+public:
+	NN(int iInputCount, int iHiddenCount, int iOutputCount);
+	~NN() { ; }
+};
+
+#endif // !NN_H
